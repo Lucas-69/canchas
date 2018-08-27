e5373271d96ded25ce64f0b9c4d7c1442dc31476
LucasRios <rioslucas97@gmail.com>
2018-08-27 15:23:43 -0300

    created reservations views

---
+++ resources/views/admin/reservations/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.template.main')
+
+@section('title','Editar Reserva')
+
+@section('content')
+
+
+<div class="container">
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
---
+++ resources/views/admin/reservations/index.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.template.main')
+
+@section('title','Lista de Reservas')
+
+@section('content')
+
+
+<div class="container">
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
