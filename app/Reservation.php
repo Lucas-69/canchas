c86c1a518b7260e6cb60e373c94d2e2d12984882
LucasRios <rioslucas97@gmail.com>
2018-08-30 19:52:34 -0300

    crud de reservas

---
+++ app/Reservation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    //
+      protected $table = "reservations";
+
+    protected $fillable = ['id','precioTotal','user_id'];
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+}
