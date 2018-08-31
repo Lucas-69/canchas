24118a563365b3d8c6fd5d06ed1f87740b46b3fd
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2018-08-31 17:09:01 -0300

    bookings views

---
+++ resources/views/admin/bookings/create.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.template.main')
+
+@section('title','New Booking')
+
+@section('content')
+@if(count($errors)>0)
+
+	<div class="alert alert-danger">
+		<ul>
+			@foreach($errors->all() as $error)
+
+				<li>
+					{{ $error}}
+				</li>
+
+			@endforeach
+		</ul>
+		
+
+	</div>
+	
+@endif
+<div class="container">
+	<h3>New Booking</h3>
+
+
+</div>
+@endsection
+
+@section('js')
+	<script>
+
+		$('.select-category').chosen({
+			placeholder_text_multiple:'Seleccione al menos 3 tags',
+			no_results_text: "Oops, no hay categoria parecida a ",
+			search_contains:true,
+
+		});
+
+		$('#trumbowyg-demo').trumbowyg();
+		$('#trumbowyg-demo2').trumbowyg();
+
+		document.getElementById("upload").onchange = function() {
+			var reader = new FileReader(); //instanciamos el objeto de la api FileReader
+
+  			reader.onload = function(e) {
+    		document.getElementById("image").src = e.target.result;
+  			};
+
+  		// read the image file as a data URL.
+  		reader.readAsDataURL(this.files[0]);
+		};
+
+		function limite_textarea(valor) {   
+    		total = valor.length;
+        	document.getElementById('cont').innerHTML = total;
+		}
+
+		
+
+		
+	</script>
+
+@endsection
---
+++ resources/views/admin/bookings/index.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.template.main')
+
+@section('title','Bookings')
+
+@section('content')
+@if(count($errors)>0)
+
+	<div class="alert alert-danger">
+		<ul>
+			@foreach($errors->all() as $error)
+
+				<li>
+					{{ $error}}
+				</li>
+
+			@endforeach
+		</ul>
+		
+
+	</div>
+	
+@endif
+<div class="container">
+
+
+</div>
+@endsection
