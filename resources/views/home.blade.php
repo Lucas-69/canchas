f23fa899667ed3b07b125aa3d7d5d5a2d3b44104
LucasRios <rioslucas97@gmail.com>
2018-09-13 12:36:28 -0300

    vista de presentacion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,15 @@
 @section('title','Home')
 
 @section('content')
+<div class="img-back">
 <div class="container">
-    <div class="text-center">
-        <h3>Altas Canchitas amiguero!</h3>
-    </div>    
-    <span class="pelotita">s</span>
+		
+	<div class="text-center">
+	    <h3>Altas Canchitas amiguero!</h3>
+	</div>    
+	<span class="pelotita"><h1>JOYA COMPADRE</h1></span>
+
+   
+</div>
 </div>
 @endsection
