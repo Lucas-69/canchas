8c571ede2ade5e1036c9c762bc2040f8e7e117fc
LucasRios <rioslucas97@gmail.com>
2018-09-14 18:00:37 -0300

    control de funcionalidad de vista presentacion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,14 @@
 @section('title','Home')
 
 @section('content')
-<div class="img-back">
-<div class="container">
-		
-	<div class="text-center">
-	    <h3>Altas Canchitas amiguero!</h3>
-	</div>    
-	<span class="pelotita"><h1>JOYA COMPADRE</h1></span>
 
-   
-</div>
+<div class="img-back">
+	<div class="container">
+			
+		<div class="text-center">
+		    <h3>Altas Canchitas amiguero!</h3>
+		</div>    
+		<span class="pelotita"><h2> Loading...</h2></span>
+	</div>
 </div>
 @endsection
